b7be19c7bb3b3bc232cb8aa55fb1f773a30e1fcc
AltESV <ishigami.daniel@gmail.com>
2023-08-24 13:46:14 +0100

    updates

--- controller.js
+++ controller.js
@@ -1,17 +1,43 @@
 const { query } = require('express');
-// const APIFeatures = require('../utils/apiFeatures')
+const dotenv = require('dotenv');
+const nodemailer = require('nodemailer');
 
+dotenv.config()
+
+
+const whitelist = ['1234', '4321']
+
+    
+    const transporter = nodemailer.createTransport({
+        service: process.env.EMAIL_SERVICE,
+        auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    });
+
+    
+    const main = async () => {
+        const info = await transporter.sendMail({
+            from: '"Daniel 👻" <daniel@myfana.com>',
+            to: "robin@myfana.com",
+            subject: "Fana NFC Activation",
+            text: "Brace yourself",
+            html: "<b>Get Staret<b>"
+        });
+    
+        console.log("Message sent: %s", info.messageId);
+    }
 
 //EXECUTE QUERY
 exports.getNFCresource = async (req, res) => {
-    
-    const whitelist = ['1234', '4321']
     
     try {
         const user = req.query.user;
 
         if(whitelist.includes(user)) {
-            return res.redirect('https://fanaverse.io')
+            await main();
+            return res.status(200).send('Email sent')
         } else {
             return res.status(403).send('Access restricted')
         }
